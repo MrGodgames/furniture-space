18bbee74cfa5fe5a43b324fa933b909262fc1749
MrGodgames <sashka.bog14@gmail.com>
2025-05-16 15:21:08 +0300

    3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPhone, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faAddressBook, faHeart, faPhone, faSearch, faShoppingCart, faUser } from '@fortawesome/free-solid-svg-icons';
 
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
     <header>
       <div className="header">
         <div className="logo">
-          <img src="./public/images/logo.jpg" alt="logo" />
+          <img src="./public/images/logo.png" alt="logo" />
         </div>
         <div className='search'>
           <p>Поиск...</p>
@@ -27,6 +27,10 @@ function App() {
           <p>8 (800) 555-35-35</p>
         </div>
         <div className="fastnav">
+        <i className='nav-button'><FontAwesomeIcon icon={faHeart} className='nav-buttonico'/></i>
+        <i className='nav-button'><FontAwesomeIcon icon={faUser} className='nav-buttonico'/></i>
+        <i className='nav-button'><FontAwesomeIcon icon={faShoppingCart} className='nav-buttonico'/></i>
+        <i className='nav-button'><FontAwesomeIcon icon={faAddressBook} className='nav-buttonico'/></i>
         </div>
       </div>
     </header>
