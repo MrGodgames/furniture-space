cf2d57d8dc6bd7cf7ae8229ce02c7fd162000efb
MrGodgames <sashka.bog14@gmail.com>
2025-05-16 14:21:52 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,27 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
+  const [isHovered, setIsHovered] = useState(false)
   return (
     <>
-    <h1>Hello World</h1>
+    <header>
+      <div className="header">
+        <div className="logo">
+          <img src="./public/images/logo.jpg" alt="logo" />
+        </div>
+        <div className='search'>
+          <p>Поиск...</p>
+          <button className={`sr-button ${isHovered ? 'hover' : ''}`}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          >
+          </button>
+        </div>
+        <div className="fastnav">
+
+        </div>
+      </div>
+    </header>
     </>
   )
 }
