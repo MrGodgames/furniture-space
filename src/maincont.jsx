d291ef634ca3abf549e59e68f505b8720211ffc6
dmitriyg0r <62027285+dmitriyg0r@users.noreply.github.com>
2025-05-19 13:45:20 +0300

    доработка сайдбара и вставка мейнконтент

---
+++ src/maincont.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './maincont.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAddressBook, faHeart, faPhone, faSearch, faShoppingCart, faUser } from '@fortawesome/free-solid-svg-icons';
+
+    function Maincont() {
+        return (
+            
+            <div className="maincont">проверка</div>
+        )
+    }
+
+export default Maincont;
