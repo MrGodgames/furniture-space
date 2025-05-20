488acefa1c6fb1c0e2f10a9fa0b9aa77535f80f4
MrGodgames <sashka.bog14@gmail.com>
2025-05-20 14:54:57 +0300

    3

--- src/hero.jsx
+++ src/hero.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import './hero.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAddressBook, faHeart, faPhone, faSearch, faShoppingCart, faUser } from '@fortawesome/free-solid-svg-icons';
+import strelkaImg from './assets/items/strelka.svg';
 
 function Hero() {
     return (
-        <div className='heroic'>
-            <img src="" alt="heroimg1" />
-        </div>
+
+        <>
+            <div className='heroic'>heroheroheroherohero
+            <img src={strelkaImg} alt="стрелка" style={{ width: 100, height: 100 }} />
+            </div>
+        </>
     )
 }
 export default Hero;
