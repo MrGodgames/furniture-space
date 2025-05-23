ecc8e2e33ce7780e734f884aab2e76c2db084284
MrGodgames <sashka.bog14@gmail.com>
2025-05-23 21:27:59 +0300

    catalog 2

--- src/pages/catalog.jsx
+++ src/pages/catalog.jsx
@@ -9,10 +9,32 @@ function Catalog() {
       <Sidebar />
       <div className="catalog-page">
         <h1>Каталог</h1>
-        <div className="catalog-container">
-          <div className="catalog-container">
-          <h1>Мега крутая мебель</h1>
-          <p>такая мебель только у тебя и у майкла джексона</p>
+        <div className="categories-container">
+          <div className="category-item">
+            <img src="https://ruminatop.ru/uploads/731d6c18-bfe2-4137-978e-b9df79600621.jpeg" alt="Мебель" />
+            <h2>Шкафы</h2>
+          </div>
+          <div className="category-item">
+            <img src="https://ruminatop.ru/uploads/8d13e5fe-cf55-4ff5-9378-770abffed59e.png" alt="Мебель" />
+            <h2>Кровати</h2>
+          </div>
+          <div className="category-item">
+            <img src="https://ruminatop.ru/uploads/731d6c18-bfe2-4137-978e-b9df79600621.jpeg" alt="Мебель" />
+            <h2>Шкафы</h2>
+          </div>
+          <div className="category-item">
+            <img src="https://ruminatop.ru/uploads/8d13e5fe-cf55-4ff5-9378-770abffed59e.png" alt="Мебель" />
+            <h2>Кровати</h2>
+          </div>
+        </div>
+        <h2>Популярные товары</h2>
+        <div className="products-container">
+          <div className="product-item">
+            <img src="https://ruminatop.ru/uploads/731d6c18-bfe2-4137-978e-b9df79600621.jpeg" alt="Мебель" />
+            <h2>Авангард</h2>
+            <p>Категория: Шкафы</p>
+            <p>Цена: 100000 руб.</p>
+            <button>Купить</button>
           </div>
         </div>
       </div>
